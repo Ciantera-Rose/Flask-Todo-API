3403b0e28cbf3981e85f115b6a8690e0675c4700
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-07 19:46:43 -0500

    adds cors policy

--- app.py
+++ app.py
@@ -8,15 +8,13 @@ import os
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 heroku = Heroku(app)
 
 env = Env()
 env.read_env()
 DATABASE_URL = env("DATABASE_URL")
-print(DATABASE_URL)
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
 basedir = os.path.abspath(os.path.dirname(__file__)) 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
